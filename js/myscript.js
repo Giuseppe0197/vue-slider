4255fc91b5b95a8abd01a2cd2d4c8544880ddc51
Giuseppe0197 <faro.g@hotmail.it>
2021-11-11 14:10:35 +0100

    impostazione lavoro con riresa classi css e html

---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+/* esercizio del carousel ma con vue js */
+
+/* andiamo a collegare il vue all'html */
+
+/* creiamo l'array con gli oggetti necessari al riempimento delle immagini, titoli e paragrafi */
+
+/* andiamo a creare una funzione per andare avanti con il click */
+
+/* andiamo a creare una funzione per tornare indietro con il click */
